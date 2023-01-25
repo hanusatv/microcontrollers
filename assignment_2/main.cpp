fca5fdbbc22eef80f63f97f403a8a66cc1bc0c4d
Hanus á Tjaldrafløtti Vestergaard <hanusatv@gmail.com>
2023-01-25 16:33:56 +0100

    Added assignment 2

---
+++ assignment_2/main.cpp
@@ -0,0 +1,42 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+#include <cstdio>
+
+#define WAIT_TIME_MS 100
+#define BLINK_TIME_MS 10
+
+DigitalOut led0(LED1);
+
+DigitalIn button(D0, PullDown);
+AnalogIn potentiometer(A0);
+PwmOut led1(D3);
+PwmOut led2(D4);
+PwmOut led3(D5);
+
+
+int main()
+{
+    led1.period_ms(BLINK_TIME_MS);
+    led2.period_ms(BLINK_TIME_MS);
+    led3.period_ms(BLINK_TIME_MS);
+
+    float potentiometer_value;
+    while (true)
+    {
+        potentiometer_value = potentiometer.read();
+        if (button.read()) {
+            led1 = led2 = led3 = 1;
+        }
+        else {
+                led1.write(potentiometer_value*3);
+                led2.write((potentiometer_value - 0.33)*3);
+                led3.write((potentiometer_value - 0.67)*3);
+        }
+        led0 = !led0;
+        thread_sleep_for(WAIT_TIME_MS);
+    }
+}
